71897017005a6521cbc63324faee7732dc589bad
Ionică Bizău <bizauionica@gmail.com>
2016-05-24 12:59:23 +0300

    Use babel programmatically

--- bin/babel-it.js
+++ bin/babel-it.js
@@ -4,6 +4,8 @@
 
 const spawn = require("spawno")
     , oargv = require("oargv")
+    , babel = require("babel-core")
+    , glob = require("glob")
     , tilda = require("tilda")
     , typpy = require("typpy")
     , queue = require("one-by-one")
@@ -12,6 +14,9 @@ const spawn = require("spawno")
     , logger = require("bug-killer")
     , fs = require("fs")
     , wJson = require("w-json")
+    , sameTime = require("same-time")
+    , es2015 = require("babel-preset-es2015")
+    , bindy = require("bindy")
     ;
 
 const BABEL_PATH = require.resolve("babel-cli/bin/babel.js");
@@ -22,13 +27,14 @@ new tilda(`${__dirname}/../package.json`, {
             name: "path"
           , opts: ["i", "input"]
           , desc: "The input file paths."
-          , default: "."
+          , default: "**/**.js"
+          , type: String
         }
       , {
             name: "path"
-          , opts: ["o", "output"]
-          , desc: "The output directory."
-          , default: "."
+          , opts: ["I", "ignore"]
+          , desc: "The files to ignore."
+          , default: "node_modules/**"
         }
       , {
             opts: ["skip-publish"]
@@ -40,34 +46,30 @@ new tilda(`${__dirname}/../package.json`, {
         }
     ]
 }).main(a =>{
-    if (!typpy(a.options.input.value, Array)) {
-        a.options.input.value = [a.options.input.value];
-    }
-
     queue([
         gitStatus
       , (next, data) => next(data.length && new Error("Please commit the changes in your git repository first."))
       , next => {
-            logger.log("Creating .babelrc.");
-            wJson(".babelrc", { presets: ["es2015"] }, next)
+            logger.log("Getting the files to update.");
+            console.log(a.options.input.value);
+            glob(a.options.input.value, { ignore: a.options.ignore.value }, next);
         }
-      , next => {
+      , (next, files) => {
             logger.log("Babelifying the things.");
-            spawn(BABEL_PATH, oargv({
-                _: a.options.input.value
-              , d: a.options.output.value
-              , ignore: "node_modules"
-            }), { _showOutput: true }, next);
+            sameTime(bindy(files, (cPath, cb) => {
+                babel.transformFile(cPath, {
+                    presets: [es2015]
+                }, (err, result) => {
+                    if (err) { return cb(err); }
+                    fs.writeFile(cPath, result.code, cb);
+                });
+            }), next);
         }
-      , next => {
-            logger.log("Removing .babelrc.");
-            fs.unlink(".babelrc", next)
-        }
-      , a.options["skip-publish"].is_provided ? null : next => {
+      , a.options.skipPublish.is_provided ? null : next => {
             logger.log("Publishing on npm.");
             spawnNpm("publish", {}, { _showOutput: true }, next);
         }
-      , a.options["skip-checkout"].is_provided ? null : next => {
+      , a.options.skipCheckout.is_provided ? null : next => {
             logger.log("Resetting the changes using git.");
             spawn("git", ["checkout", "."], next);
         }
@@ -78,4 +80,3 @@ new tilda(`${__dirname}/../package.json`, {
         logger.log("Done.");
     });
 });
-
